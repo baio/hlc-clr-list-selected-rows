0ee4663afd5cb6029b3819aa7e09eccda446c99d
baio <max.putilov@gmail.com>
2019-07-07 15:38:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hlc-clr-list-selected-rows
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hlc-clr-list-selected-rows)
---
+++ src/app/planets-table.component.ts
@@ -0,0 +1,54 @@
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { Table, TableDescription, HlcClrTableComponent } from '@ng-holistic/clr-list';
+import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { SWAPIService } from './swapi.service';
+import { AppModels } from './app.models';
+
+// Provide table UI definition in js object
+const table: TableDescription = {
+  cols: [
+    {
+      id: 'name',
+      title: 'Name',
+      sort: true
+    },
+    {
+      id: 'population',
+      title: 'Population',
+      sort: false
+    }
+  ]
+};
+
+@Component({
+  selector: 'my-planets-table',
+  template: '<hlc-clr-table [selectedRows]="selectedRows" (selectedRowsChanged)="onSelectedRowsChanged($event)" [table]="table" [dataProvider]="dataProvider"></hlc-clr-table>',
+  styleUrls: ['./palnets-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
+})
+export class TableComponent {
+  readonly table = table;
+  readonly dataProvider: Table.Data.DataProvider;
+  selectedRows: string[] = [];
+
+  @ViewChild(HlcClrTableComponent, { static: true }) tableComponent: HlcClrTableComponent;
+
+  constructor(swapi: SWAPIService) {
+    this.dataProvider = {
+      load(state: any) {
+        return swapi.planets(state).pipe(
+          tap(console.log),
+          catchError(err => {
+            return throwError('SWAPI return error');
+          })
+        );
+      }
+    };
+  }
+
+  onSelectedRowsChanged(selectedRows: AppModels.Planet[]) {
+    console.log('Selected rows', selectedRows);
+  }
+}
